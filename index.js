f53687e1a4b6c3fe018acbdd8ffd6857153f5fdc
Aksel <aksel.muur@voco.ee>
2025-10-16 10:55:19 +0300

    asd

--- index.js
+++ index.js
@@ -1,169 +1,21 @@
-//console.log("Hello!");
-
-//console.log("I like pizza!");
-
-//window.alert("This is an alert!");
-//window.alert("I like pizza!");
-
-// This is a comment
-
-/* This is also a comment
-    Group comment control + K + C
-*/
-
-/* 1 TUND!!!!!!!!!!!!!!!!
-document.getElementById("myH1").textContent = "Hello!";
-document.getElementById("myP").textContent = "I like pizza!";
-
-let x;
-x = 123;
-
-console.log(x);
-
-let age = 25;
-let price = 10.99;
-
-console.log(`Your are ${age} years old`);
-console.log(`The price is $${price}`);
-
-
-
-let online = false;
-let forSale = true;
-let isStudent = true;
-
-console.log("Bro is online");
-console.log(forSale);
-console.log(isStudent);
-*/
-
-// 2 TUND !!!!!!!!!!!!!!! variables
-/*
-let fullName = "Bro Code";
-let age = 25;
-let isStudent = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `Your are ${age} yeas old`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
-
-console.log(document);
-console.log(window);
-
-console.log(typeof isStudent);
-*/
-// arithmetic operators + - * / % ** ++ --
-
-/*
-let students = 16;
-
-//students = students + 1;             //LIIDA
-//students = students - 2;             //LAHUTA
-//students = students * 2;             //KORRUTA
-//students = students / 2;             //JAGA   
-//students = students ** 2;            //ASTMES
-//let extraStudents = students % 3;    //JÄÄK
-//students +=2;
-//students++
-//students
-Math.sqrt(students);           //RUUTJUUR 
-console.log(Math.sqrt(students));
-*/
-
-//let result = 1+2*3+4**2;
-//console.log(result);
-
-// How to accept user input
-/*
-let username;
-
-//username = window.prompt("Enter your username: ");
-console.log(username);
-document.getElementById("mySubmit").onclick = function() {
-    username = document.getElementById("myText").value;
-  //  console.log(username);
-  document.getElementById("myH1").textContent = `Hello ${username}`;
-}
-
-*/
-
-// type conversion
-/*
-//let age = window.prompt("How old are you?");
-//age = Number(age);
-//age +=1;
-//console.log(`You are ${age} years old`);
-//console.log(age,typeof age  );
-
-let x = "pizza";
-let y = "pizza";
-let z = "pizza";
-
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
-
-*/
-
-// constants
-/*
-let PI = 3.14159;
-let radius;
-let circumference;
-
-//radius = window.prompt("Enter the radius of a circle: ");
-//radius = Number(radius);
-
-//circumference = 2 * pi * radius;
-
-//console.log(`The circumference is ${circumference}`);
-
-document.getElementById("mySubmit").onclick = function() {
-    radius = document.getElementById("myText").value;
-    radius = Number(radius);
-    circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = `The circumference is ${circumference} cm`;
-}
-
-*/
-
 // COUNTER PROGRAM
-/*
+
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 let count = 0;
 
-increaseBtn.onclick = function() {
-    count++;
-    countLabel.textContent = count;
-}
+increaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
 
-decreaseBtn.onclick = function() {
-    count--;
-    countLabel.textContent = count;
-}
+decreaseBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
 
-resetBtn.onclick = function() {
-    count=0;
-    countLabel.textContent = count;
-}
-
-*/
-
-// MATH
-
-console.log(Math.PI);
-console.log(Math.sqrt(16));
-console.log(Math.pow(2, 4));
-console.log(Math.E);
-
-let x = 3.21;
-let y = 2;
-let z;
-z = Math.round(x);
-console.log(z);
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
