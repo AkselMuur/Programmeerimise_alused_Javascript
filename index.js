8575606f4f4075bf97e6e5f7b82c02aea9f43aae
Aksel <aksel.muur@voco.ee>
2025-10-16 08:40:50 +0300

    tund

---
+++ index.js
@@ -0,0 +1,27 @@
+//console.log("Hello!");
+
+//console.log("I like pizza!");
+
+//window.alert("This is an alert!");
+//window.alert("I like pizza!");
+
+// This is a comment
+
+/* This is also a comment
+    Group comment control + K + C
+*/
+
+document.getElementById("myH1").textContent = "Hello!";
+document.getElementById("myP").textContent = "I like pizza!";
+
+let x;
+x = 123;
+
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+
+console.log(`Your are ${age} years old`);
+console.log(`The price is $${price}`);
+
