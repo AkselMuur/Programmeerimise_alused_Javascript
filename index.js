23965b5144811838d2dc00a8903783d400769e74
Paan Kaur <paan.kaur@gmail.com>
2025-10-23 10:52:39 +0300

    123

--- index.js
+++ index.js
@@ -188,4 +188,31 @@ console.log(max);
 
 */
 
-// RANDOM NUMBER GENERATOR
+// terenary operator
+
+/*
+let age = 18;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+
+console.log(message);
+*/
+/*
+let time = 16;
+let greeting = time < 12 ? "Good morning" : "Good afternoon";
+
+console.log(greeting);
+
+*/
+
+/*
+let isStudent = true;
+let message = isStudent ? "You are a student":"You are not a student";
+
+console.log(message);
+*/
+
+let purchaseAmount = 100;
+
+let discount = purchaseAmount >= 100 ? 10:0;
+console.log(`Your total is $${purchaseAmount-purchaseAmount*(discount/100)}`);
+
