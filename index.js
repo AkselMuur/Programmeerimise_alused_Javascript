bcbb8f9315c5b211ee8c7e6107dfb4f3510dfce9
Paan Kaur <paan.kaur@gmail.com>
2025-10-23 15:17:30 +0300

    Kodu2

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-let x = prompt("Sisesta vanus");
+function vanus() {
+  let x = prompt("Sisesta vanus");
 
-if (isNaN(x)) {
-  alert("Ei sisestatud number!");
-  x = prompt("Sisesta vanus");
-} else if (x < 0) {
-  alert("Ei ole negatiivseid vanuseid");
-} else if (x >= 0 && x <= 6) {
-  alert("Kasva veel veidi");
-} else if (x >= 99) {
-  alert("Söögilt maha arvatud");
-} else if (x >= 7 && x <= 16) {
-  alert("Söögi aeg on kell 10.00");
-} else if (x >= 17 && x <= 20) {
-  alert("Söögi aeg on kell 11.00");
-} else if (x >= 21 && x <= 99) {
-  alert("Söögi aeg on kell 12.00");
+  if (isNaN(x)) {
+    alert("Ei sisestatud number!");
+    vanus();
+  } else if (x < 0) {
+    alert("Ei ole negatiivseid vanuseid");
+  } else if (x >= 0 && x <= 6) {
+    alert("Kasva veel veidi");
+  } else if (x >= 99) {
+    alert("Söögilt maha arvatud");
+  } else if (x >= 7 && x <= 16) {
+    alert("Söögi aeg on kell 10.00");
+  } else if (x >= 17 && x <= 20) {
+    alert("Söögi aeg on kell 11.00");
+  } else if (x >= 21 && x <= 99) {
+    alert("Söögi aeg on kell 12.00");
+  }
 }
+
+vanus();
