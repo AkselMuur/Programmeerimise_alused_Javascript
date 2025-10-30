70f8003fd326eb4cf3c617c21e848e370c40174a
Paan Kaur <paan.kaur@gmail.com>
2025-10-30 11:22:23 +0200

    fds

--- index.js
+++ index.js
@@ -939,3 +939,46 @@ function rollDice() {
   DiceImages.innerHTML = images.join("");
 }
 */
+
+//          CALLBACK = function that passes an argument to another function !!!!!
+
+/*
+hello(goodbye);
+
+function hello(callback) {
+  console.log("Hello!"); 
+  callback();
+}
+function wait() {
+  console.log("Wait!");
+}
+
+function leave() {
+  console.log("Leave!");
+}
+
+function goodbye() {
+  console.log("Goodbye!");
+}
+*/
+
+/*
+
+sum(displayConsole,1,2)
+sum(displayPage,1,2)
+
+
+function sum(callback,x,y){
+let result = x+y
+callback(result)
+}
+
+function displayConsole(result){
+  console.log(result)
+}
+
+function displayPage(result){
+  document.getElementById("myH1").textContent=result
+}
+
+*/
