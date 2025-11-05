985a5b2f0e1081a22c6961d72b002589dc496a09
Paan Kaur <paan.kaur@gmail.com>
2025-11-05 10:53:39 +0200

    sa

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ function rollDice() {
   //console.log(diceResult);
   const values = [];
   const images = [];
+  let sum = 0;
   for (let i = 0; i < numofDice; i++) {
     let max = 6;
     // console.log(Math.floor(Math.random() * max + 1));
     const value = Math.floor(Math.random() * max + 1);
     values.push(value);
+    sum += value;
     images.push(`<img src="Dice/${value}.png" alt="Dice ${value}">`);
   }
   //console.log(values);
   //console.log(images);
 
-  DiceResult.textContent = `dice:${values.join(", ")}`;
+  DiceResult.textContent = `Sum: ${sum}, dice:${values.join(", ")}`;
   DiceImages.innerHTML = images.join("");
+}
+
+function reset() {
+  location.reload();
 }
