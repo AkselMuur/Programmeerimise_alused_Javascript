fb2f498b5993f72348fe6421518fc1c86c70ad6f
Aksel Müür <Aksel.Muur@Voco.ee>
2025-11-06 22:56:03 +0200

    Cameltottitle

--- index.js
+++ index.js
@@ -982,3 +982,34 @@ function displayPage(result){
 }
 
 */
+
+
+// Camel to titel
+
+function camelToTitleCase(words){
+words = words.trim().charAt(0).toUpperCase() + words.trim().slice(1)
+let i = 0;
+words = words.split("")
+for (i; i < words.length; i += 1) {
+  
+  if (i>0&&i<words.length-1&&words[i]===words[i].toUpperCase()&&(
+    words[i-1]!=words[i-1].toUpperCase()||
+    words[i+1]!=words[i+1].toUpperCase())){
+        words.splice(i,0," ");
+        i+=1;
+  }
+  else{
+    
+  }
+}
+words = words.join("");
+console.log(words);
+}
+
+
+camelToTitleCase("insertHTML");
+camelToTitleCase("insertAdjacentHTML");
+camelToTitleCase("firstHTMLElement");
+camelToTitleCase("releaseDate");
+camelToTitleCase("");
+
